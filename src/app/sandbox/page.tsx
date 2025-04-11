c5975f03aee666653be5fa6f7cc2a8315f1f12f5
dambar08 <notdambar@gmail.com>
2025-04-12 00:04:27 +0800

    mock datab

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,11 +1,35 @@
+import { mockFiles, mockFolders } from "@/lib/mock-data";
+import { db } from "@/server/db";
+import { folders_table, files_table } from "@/server/db/schema";
+
 export default function SandboxPage() {
   return (
     <div className="flex flex-col gap-4">
       Seed Function
-      <form action={async () => {
-        "use server"
-        console.log("")
-      }}></form>
+      <form
+        action={async () => {
+          "use server";
+          await db.insert(folders_table).values(
+            mockFolders.map((folder, index) => ({
+              id: index + 1,
+              name: folder.name,
+              parent: index !== 0 ? 1 : null,
+            })),
+          );
+          await db.insert(files_table).values(
+            mockFiles.map((file, index) => ({
+              id: index + 1,
+              name: file.name,
+              size: 5000,
+              url: file.url,
+              parent: (index % 3) + 1,
+              mimeType: "application/pdf",
+            })),
+          );
+        }}
+      >
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
